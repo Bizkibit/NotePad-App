a681cc1deb458086ade90eb74f0683e35cb65a63
Farshid HM <fhmanesh@gmail.com>
2017-11-03 11:57:55 -0700

    added delete button

--- public/javascripts/publicApp.js
+++ public/javascripts/publicApp.js
@@ -1,8 +1,19 @@
 window.onload = function()  {
 
   let form = document.querySelector('#new-note-form');
-  let board = document.querySelector('.board');
-  getNotes().then(renderNotes);
+  // let board = document.querySelector('.board');
+  // let notes;
+  // getNotes()
+  // .then(renderNotes)
+  // .then(function() {
+  //   notes = document.querySelectorAll('.note');
+  //   debugger;
+  //   notes.forEach( note => note.addEventListener('mousedown', () => {
+  //     alert('cunt')
+  //   }))
+  // })
+  displayNotes();
+
 
   form.addEventListener('submit', function(e) {
   	e.preventDefault();
@@ -11,8 +22,7 @@ window.onload = function()  {
   	fetch('/notes', {
   		method: 'POST',
   		body: fData
-  	}).then(() => getNotes())
-      .then(renderNotes);
+  	}).then(displayNotes);
     currentTarget.reset();
   });
 
@@ -29,8 +39,32 @@ function renderNotes(notes)  {
       <div class="note" id="note-${note.id}">
         <h3>${note.title}</h3>
         <p>${note.content}</p>
+        <button class='delete'>delete</button>
       </div>
     `
   }).join('')
   board.innerHTML = notesHTML;
+}
+
+function displayNotes() {
+  getNotes()
+  .then(renderNotes)
+  .then(function() {
+    // let notes = document.querySelectorAll('.note');
+    let deleteButtons = document.querySelectorAll('.delete');
+    deleteButtons.forEach(deleteButton => deleteButton.addEventListener('click', deleteNotes));
+    // notes.forEach( note => note.addEventListener('mousedown', () => {
+    //   alert('cunt')
+    // }))
+  })
+}
+
+function deleteNotes(e)  {
+  e.stopPropagation();
+  let {currentTarget} = e;
+  let id = currentTarget.parentElement.id.split('-')[1];
+  fetch(`/notes/${id}`, {
+    method: 'delete'
+  })
+  .then(displayNotes);
 }
