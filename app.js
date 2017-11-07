ab40037300d87bd7ff0bb23db3846373883913cf
Farshid HM <fhmanesh@gmail.com>
2017-11-06 18:53:37 -0800

    testing heroku recomandation

--- app.js
+++ app.js
@@ -15,6 +15,24 @@ const users = require('./routes/users');
 
 const app = express();
 
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
